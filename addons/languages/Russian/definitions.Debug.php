0de0dcbfe68bf60b092c7839270a2caf232d177c
Roman <64708726+madromas@users.noreply.github.com>
2023-08-08 15:51:48 -0400

    Delete addons/languages/Russian directory

--- addons/languages/Russian/definitions.Debug.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	$definitions["MySQL queries"] = "Запросы MySQL";
-	$definitions["Page loaded in %s seconds"] = "Страница загружалась в течение % секунд";
-	$definitions["POST + GET + FILES information"] = "Информация POST + GET + FILES";
-	$definitions["SESSION + COOKIE information"] = "Информация SESSION + COOKIE";
--- addons/languages/Russian/definitions.SMTP.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$definitions["Server"] = "Сервер";
-	$definitions["Port"] = "Порт";
-	$definitions["Authentication"] = "Аутентификация";
-	$definitions["Normal"] = "Нормальный";
-	$definitions["TLS"] = "TLS";
-	$definitions["SSL"] = "SSL";
