43f19d1182b178a22281e88ff976db456cc3c9fa
Roman <64708726+madromas@users.noreply.github.com>
2023-08-14 11:12:44 -0400

    Add files via upload

---
+++ addons/languages/Russian/definitions.HideTag.php
@@ -0,0 +1,5 @@
+<?php
+$definitions["hidden.YouMustBeLoggedIn"] = "Вы должны авторизоваться, для возможности просмотра этого контента.";
+$definitions["hidden.YouMustHavePosts"] = "Вы должны иметь $1 сообщений на форуме, для возможности просмотра этого контента.";
+$definitions["hidden.YouMustBeInGroups"] = "Вы должны состоять в группе « $1 », для возможности просмотра этого контента.";
+$definitions["hidden.ContentForUsers"] = "Приватный контент только для пользователя: $1.";
