1ef55e3cbc3f62ad1833a555b992e38c3473b28d
Roman <64708726+madromas@users.noreply.github.com>
2023-08-14 07:58:48 -0400

    Update font awesome to 6.4.2

--- core/views/default.master.php
+++ core/views/default.master.php
@@ -30,7 +30,7 @@ if (!defined("IN_ESOTALK")) exit;
     <link rel="shortcut icon" type="image/png" href="<?php echo URL('',true); ?>favicon.png" />
   <?php } ?> 
   <link rel="manifest" href="/manifest.json">
-<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v6.4.2/css/all.css">
 <link rel="shortcut icon" href="/favicon.png" />
 <link rel="stylesheet" type="text/css" href="/fancybox/fancybox.css">
 <link type="text/css" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500&display=swap" rel="stylesheet">
