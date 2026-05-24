197dac86934e30ca979e0ac67e62e0cbb81f6f83
MadWay Inc. <64708726+madromas@users.noreply.github.com>
2026-05-24 16:27:19 -0400

    Add files via upload

--- addons/skins/Dark/views/mobile.master.php
+++ addons/skins/Dark/views/mobile.master.php
@@ -86,9 +86,9 @@ $(function(){ //on document ready
 
 <li class="item-Links mode-switch">
      <button  class="sel-dark-toggle" id="toggle-darkmod">
-   <svg class="i__moon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="butt" stroke-linejoin="bevel"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+   <svg class="i__moon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="butt" stroke-linejoin="bevel"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
    
-    <svg  class="i__sun" xmlns="http://www.w3.org/2000/svg" style="display:none;" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    <svg  class="i__sun" xmlns="http://www.w3.org/2000/svg" style="display:none;" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
           <circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/>  
          </svg></button></li>
 		 
