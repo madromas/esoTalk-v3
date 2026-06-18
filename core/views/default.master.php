26d3be61286198750db63366d09773e71dc1337f
MadWay Inc. <64708726+madromas@users.noreply.github.com>
2026-06-18 14:58:37 -0400

    Add files via upload

--- core/views/default.master.php
+++ core/views/default.master.php
@@ -29,11 +29,9 @@ if (!defined("IN_ESOTALK")) exit;
     <meta property="og:image" content="<?php echo URL('',true); ?>uploads/logo.png" />
   <?php } ?>
   <?php if(file_exists('favicon.png')) { ?>
-  
     <link rel="shortcut icon" type="image/png" href="<?php echo URL('',true); ?>favicon.png" />
   <?php } ?>
 <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v6.4.2/css/all.css">
-<link rel="shortcut icon" href="/favicon.png" />
 <link rel="stylesheet" type="text/css" href="/fancybox/fancybox.css">
 <link type="text/css" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500&display=swap" rel="stylesheet">
 
