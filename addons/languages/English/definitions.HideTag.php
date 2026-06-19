1977e803d9f8fb5544cf7f4e7d77fddaf4109f94
MadWay Inc. <64708726+madromas@users.noreply.github.com>
2026-06-19 18:41:14 -0400

    Add files via upload

--- addons/languages/English/definitions.HideTag.php
+++ addons/languages/English/definitions.HideTag.php
@@ -1,5 +1,6 @@
 <?php
 $definitions["hidden.YouMustBeLoggedIn"] = "You must log in to be able to view this content.";
-$definitions["hidden.YouMustHavePosts"] = "You must have $1 of forum posts to be able to view this content.";
-$definitions["hidden.YouMustBeInGroups"] = "You must be a member of the « $1 » group to be able to view this content.";
-$definitions["hidden.ContentForUsers"] = "Private user-only content: $1.";
+$definitions["hidden.YouMustHavePosts"] = "You must have at least %s of forum posts to be able to view this content.";
+$definitions["hidden.YouMustBeInGroups"] = "You must be a member of the « %s » group to be able to view this content.";
+$definitions["hidden.ContentForUsers"] = "Private user-only content: %s.";
+$definitions["hidden.YouMustHaveRep"] = "You must have at least %s Reputation Points to be able to view this content.";
